684bc855fdc2650fe4099f8b8af7c73954a63326
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-03-07 20:28:06 +0530

    Login and register is successfull with Postman testing!!

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectToSocket } from "./controllers/socketManager.js";
+import userRoutes from "./routes/users.routes.js"
 
 dotenv.config();
 
@@ -22,6 +23,8 @@ app.set("port" , (process.env.PORT || 3000));
 app.use(cors());
 app.use(express.json({limit : "40kb"}));
 app.use(express.urlencoded({limit : "40kb" , extended : true}))
+
+app.use("/api/v1/users" , userRoutes);
 
 const start = async ()=>{
     const connectionDb = await mongoose.connect(URI);
